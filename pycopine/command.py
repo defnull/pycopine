318ccc7af5e0eba6eba4d5a71ccd9cd3b2f1a5bc
Marcel Hellkamp <marc@gsites.de>
2013-05-08 20:04:48 +0200

    Commands now add themselve to their command directory.

--- pycopine/command.py
+++ pycopine/command.py
@@ -1,23 +1,57 @@
+__all__ = 'CommandMeta Command'.split()
 
 def NotImplementedMethod(*a, **ka):
     raise NotImplementedError("Method not implemented")
 
+
+
+class CommandDirectory(object):
+    ''' A collection of commands '''
+    def __init__(self):
+        self.commands = {}
+    
+    def add_command(self, CommandClass):
+        if not issubclass(CommandClass, Command):
+            raise ValueError("Command classes must inherit from Command, or "\
+                             "at least use CommandMeta as their type. %r" % CommandClass)
+        name = CommandClass.__name__
+        if name in self.commands:
+            if self.commands[name] is CommandClass:
+                return
+            raise ValueError("Command names must be unique.")
+        self.commands[name] = CommandClass
+    
+    def __contains__(self, other):
+        return other in self.commands or other in self.commands.values()
+    
+    def clear(self):
+        self.commands.clear()
+
+
 class CommandMeta(type):
+    ''' Metaclass for all commands. The magic happens here. '''
 
-   @classmethod
-   def __prepare__(mcl, name, bases):
-       return dict()
+    @classmethod
+    def __prepare__(mcl, name, bases):
+        return dict()
 
-   def __new__(mcl, name, bases, ns):
-       if ns['__module__'] == __name__ and name == 'Command':
-          return super().__new__(mcl, name, bases, dict(ns))
-       if ns.get('run', NotImplementedMethod) is NotImplementedMethod:
-           raise NotImplementedError()
-       return super().__new__(mcl, name, bases, dict(ns))
+    def __new__(mcl, name, bases, ns):
+        if ns['__module__'] == __name__ and name == 'Command':
+            return super().__new__(mcl, name, bases, dict(ns))
+
+        if ns.get('run', NotImplementedMethod) is NotImplementedMethod:
+            raise NotImplementedError()
+
+        cls = super().__new__(mcl, name, bases, dict(ns))
+        cls.pycopine_directory.add_command(cls)
+        return cls
+
+
 
 class Command(object, metaclass=CommandMeta):
+    ''' Base class for all user defined commands. '''
+    pycopine_directory = CommandDirectory()
     run      = NotImplementedMethod
     fallback = NotImplementedMethod
-    
 
 
--- pycopine/test/test_command.py
+++ pycopine/test/test_command.py
@@ -1,12 +1,22 @@
 from pycopine import Command
 from nose.tools import raises
 
-@raises(NotImplementedError)
-def test_not_implemented():
-    class TestCommand(Command):
-        pass
+class TestCommand(object):
 
-def test_minimal_command():
-    class TestCommand(Command):
-        def run(self): pass
+    def tearDown(self):
+        Command.pycopine_directory.clear()
+
+    @raises(NotImplementedError)
+    def test_not_implemented(self):
+        class TestCommand(Command):
+            pass
+
+    def test_minimal_command(self):
+        class TestCommand(Command):
+            def run(self): pass
+
+    def test_command_in_directory(self):
+        class TestCommand(Command):
+            def run(self): pass
+        assert TestCommand in TestCommand.pycopine_directory
 
