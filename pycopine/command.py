61289793cd845243a8ccb361f77dabd5413ff84c
Marcel Hellkamp <marc@gsites.de>
2013-05-09 00:38:19 +0200

    Working out the final API.
    - Command now implements concurrent.futures.Future
    - Introduced CommandGroup, removed CommandDirectory
    - Lots of Tests

--- pycopine/command.py
+++ pycopine/command.py
@@ -1,39 +1,76 @@
+import concurrent.futures
+import logging
+
 __all__ =  ['Command', 'CommandMeta']
-__all__ += ['CommandDirectory']
-__all__ += ['CommandError', 'CommandSetupError', 'CommandTypeError', 'CommandNameError']
+__all__ += ['CommandGroup']
+__all__ += ['CommandError', 'CommandSetupError', 'CommandTypeError',
+            'CommandNameError', 'CommandCancelledError', 'CommandIntegrityError']
 
-
-
-def NotImplementedMethod(*a, **ka):
-    raise NotImplementedError("Method not implemented")
+class NotImplementedCallable(object):
+    def __call__(self, *a, **ka):
+        raise NotImplementedError("Method not implemented")
+NotImplementedMethod = NotImplementedCallable()
 
 class CommandError(Exception): pass
 class CommandSetupError(CommandError): pass
+class CommandIntegrityError(CommandError): pass
 
-class CommandTypeError(CommandSetupError, TypeError): pass
-class CommandNameError(CommandSetupError): pass
+class CommandTypeError(CommandError, TypeError): pass
+class CommandNameError(CommandError): pass
 
-class CommandDirectory(object):
-    ''' A collection of commands '''
-    def __init__(self):
+class CommandCancelledError(concurrent.futures.CancelledError, CommandError): pass
+class CommandTimeoutError(concurrent.futures.TimeoutError, CommandError): pass
+
+
+
+
+class CommandGroup(object):
+    __instances = dict()
+
+    @staticmethod
+    def clear_all():
+        for group in CommandGroup.__instances.values():
+            group.clear()
+        CommandGroup.__instances.clear()
+
+    def __new__(cls, name='default'):
+        if name not in cls.__instances:
+            obj = super(CommandGroup, cls).__new__(cls)
+            cls.__instances[name] = obj
+        return cls.__instances[name]
+
+    def __init__(self, name='default'):
+        if 'name' in self.__dict__:
+            return
+        self.name = name
         self.commands = {}
+        self.logger = logging.getLogger(name)
+        self.default_executor = concurrent.futures.ThreadPoolExecutor(4)
 
-    def add_command(self, CommandClass):
-        if not issubclass(CommandClass, Command):
-            raise CommandTypeError("Command classes must inherit from Command, or "\
-                             "at least use CommandMeta as their type. %r" % CommandClass)
+    def submit(self, command):
+        return self.get_executor(command.pool).submit(command._run)
+
+    def _register(self, CommandClass):
+        assert issubclass(CommandClass, Command)
+        assert not isinstance(CommandClass.group, CommandGroup)
+        assert CommandClass.group == self.name
+
         name = CommandClass.__name__
         if name in self.commands:
-            if self.commands[name] is CommandClass:
-                return
-            raise CommandNameError("Command names must be unique.")
+            raise CommandNameError("Command names must be unique per group.")
         self.commands[name] = CommandClass
-    
+        CommandClass.group  = self
+        CommandClass.logger = self.logger.getChild(name)
+
+    def get_executor(self, name):
+        return self.default_executor
+
     def __contains__(self, other):
         return other in self.commands or other in self.commands.values()
-    
+
     def clear(self):
         self.commands.clear()
+
 
 
 class CommandMeta(type):
@@ -50,16 +87,133 @@ class CommandMeta(type):
         if ns.get('run', NotImplementedMethod) is NotImplementedMethod:
             raise NotImplementedError("Commands must implement run().")
 
-        cls = super().__new__(mcl, name, bases, dict(ns))
-        cls.pycopine_directory.add_command(cls)
+        cls = type.__new__(mcl, name, bases, dict(ns))
+        CommandGroup(cls.group)._register(cls)
         return cls
 
 
 
 class Command(object, metaclass=CommandMeta):
-    ''' Base class for all user defined commands. '''
-    pycopine_directory = CommandDirectory()
-    run      = NotImplementedMethod
+    ''' Base class for all user defined commands. The base class implements the
+        (public) api of concurrent.futures.Future and adds some other methods.
+
+        Subclasses MUST implement run() and MAY implement fallback() and/or
+        cleanup(). Additional methods or attributes should be avoided or
+        made private (prefixed with `__`).
+    '''
+
+    #: Command groups share configuration and defaults.
+    group = 'default'
+    #: The executor pool that is responsible for this command
+    pool  = 'default'
+
+    run = NotImplementedMethod
     fallback = NotImplementedMethod
+    def cleanup(self): pass
+
+    def __init__(self, *a, **ka):
+        self.arguments = a, ka
+        self.__cancelled = False
+        self.__future = None
+
+    def _run(self):
+        a, ka = self.arguments
+        log   = self.logger
+
+        try:
+            return self.run(*a, **ka)
+        except Exception as e:
+            log.warning("Command failed")
+            log.exception("Command  exception: %r", e)
+            if self.fallback is NotImplementedMethod:
+                raise
+            try:
+                return self.fallback(e)
+            except Exception as e2:
+                log.error("Command fallback failed, too.")
+                log.exception("Fallback exception: %r", e2)
+                raise e
+        finally:
+            try:
+                self.cleanup()
+            except Exception as e:
+                log.error("Command cleanup failed.")
+                log.exception("Command exception: %r", e)
+
+    def submit(self):
+        if self.__future:
+            raise CommandIntegrityError("Command submitted twice.")
+        if self.__cancelled:
+            raise CommandCancelledError("Submit on cancelled command.")
+        self.__future = self.group.submit(self)
+        return self
+
+    def cancel(self):
+        ''' Attempt to cancel the call. If the call is currently being executed
+            and cannot be cancelled then the method will return False, otherwise
+            the call will be cancelled and the method will return True. '''
+        self.__cancelled = True
+        return self.__future.cancel() if self.__future else True
+
+    def cancelled(self):
+        ''' Return True if the call was successfully cancelled.'''
+        return self.__future.cancelled() if self.__future else self.__cancelled
+
+    def running(self):
+        ''' Return True if the call is currently being executed and cannot be
+            cancelled.'''
+        return self.__future and self.__future.running()
+
+    def done(self):
+        ''' Return True if the call was successfully cancelled or finished
+            running.'''
+        return self.__future.done() if self.__future else self.__cancelled
+
+    def result(self, timeout=None):
+        ''' Return the value returned by run(). If run() failed with an
+            exception and fallback() is defined, the the fallback result is
+            returned. If fallback() is not defined or fails too, this method
+            will raise the same exception as run().
+
+            If the call hasn’t yet completed then this method will wait up to
+            timeout seconds. If the call hasn’t completed in timeout seconds,
+            then a TimeoutError will be raised. timeout can be an int or float.
+            If timeout is not specified or None, there is no limit to the wait
+            time.
+
+            If the future is cancelled before completing then CancelledError
+            will be raised.'''
+        if not self.__future: self.submit()
+        return self.__future.result(timeout)
+
+    def exception(self, timeout=None):
+        ''' Return the exception raised by run(). If the call hasn’t yet
+            completed then this method will wait up to timeout seconds. If the
+            call hasn’t completed in timeout seconds, then a TimeoutError will
+            be raised. timeout can be an int or float. If timeout is not
+            specified or None, there is no limit to the wait time.
+
+            If the future is cancelled before completing then CancelledError
+            will be raised.
+
+            If the call completed without raising, None is returned.'''
+        if not self.__future: self.submit()
+        return self.__future.exception(timeout)
+
+    def add_done_callback(self, fn):
+        ''' Attaches the callable fn to the future. fn will be called, with the
+            future as its only argument, when the future is cancelled or
+            finishes running.
+
+            Added callables are called in the order that they were added and are
+            always called in a thread belonging to the process that added them.
+            If the callable raises a Exception subclass, it will be logged and
+            ignored. If the callable raises a BaseException subclass, the
+            behavior is undefined.
+
+            If the future has already completed or been cancelled, fn will be
+            called immediately.'''
+        if not self.__future: self.submit()
+        return self.__future.add_done_callback(fn)
 
 
--- pycopine/test/test_command.py
+++ pycopine/test/test_command.py
@@ -1,51 +1,206 @@
-from pycopine import Command, CommandDirectory, CommandNameError
+from pycopine import *
 from nose.tools import raises
-
+import threading
 
 class TestCommand(object):
 
     def tearDown(self):
-        Command.pycopine_directory.clear()
+        CommandGroup.clear_all()
 
     @raises(NotImplementedError)
-    def test_not_implemented(self):
-        class TestCommand(Command):
+    def test_run_not_implemented(self):
+        class MyCommand(Command):
             pass
 
+    @raises(NotImplementedError)
+    def test_fallback_not_implemented_but_called(self):
+        class MyCommand(Command):
+            def run(self): pass
+        MyCommand().fallback()
+
     def test_minimal_command(self):
-        class TestCommand(Command):
+        class MyCommand(Command):
             def run(self): pass
 
 
-class TestCommandDirectory(object):
-
-    def setUp(self):
-        self.dir = CommandDirectory()
+class TestCommandGroups(object):
 
     def tearDown(self):
-        Command.pycopine_directory.clear()
+        CommandGroup.clear_all()
 
     def test_default(self):
-        class TestCommand(Command):
+        class MyCommand(Command):
             def run(self): pass
-        assert TestCommand in Command.pycopine_directory
-        assert TestCommand in TestCommand.pycopine_directory
+        assert MyCommand in CommandGroup()
+        assert MyCommand in MyCommand.group
 
     def test_explicit(self):
-        class TestCommand(Command):
-            pycopine_directory = self.dir
+        class MyCommand(Command):
+            group = 'test'
             def run(self): pass
 
-        assert TestCommand in self.dir
-        assert TestCommand.pycopine_directory is self.dir
-        assert TestCommand not in Command.pycopine_directory
+        assert MyCommand in CommandGroup('test')
+        assert MyCommand.group is CommandGroup('test')
+        assert MyCommand not in CommandGroup()
+
+    def test_unique_per_group(self):
+        class MyCommand(Command):
+            def run(self): pass
+
+        class MyCommand(Command):
+            group = 'test'
+            def run(self): pass
 
     @raises(CommandNameError)
-    def test_NotUnique(self):
-        class TestCommand(Command):
-            pycopine_directory = self.dir
+    def test_not_unique(self):
+        class MyCommand(Command):
             def run(self): pass
 
-        class TestCommand(Command):
-            pycopine_directory = self.dir
+        class MyCommand(Command):
             def run(self): pass
+
+
+
+class TestCommandRunnable(object):
+
+    def tearDown(self):
+        CommandGroup.clear_all()
+    
+    def test_sync_execute(self):
+        class MyCommand(Command):
+            def run(self, value): return value
+        
+        assert MyCommand(5).result() == 5
+        assert MyCommand(6).result() == 6
+
+    def test_async_execute(self):
+        wakeup = threading.Event()
+        try:
+            class MyCommand(Command):
+                def run(self, value):
+                    wakeup.wait()
+                    return value
+
+            command = MyCommand(5) 
+            assert not command.running()
+            assert not command.done()
+
+            future = command.submit()
+            assert command is future
+            assert command.running()
+            assert not command.done()
+
+            wakeup.set()
+
+            assert command.result() == 5
+            assert not command.running()
+            assert command.done()
+        finally:
+            wakeup.set()
+
+    def test_fallback(sefl):
+        class MyCommand(Command):
+            def run(self, value): return 10/value
+            def fallback(self, e): return 0
+        
+        assert MyCommand(2).result() == 5
+        assert MyCommand(0).result() == 0
+
+    @raises(ZeroDivisionError)
+    def test_no_fallback(sefl):
+        class MyCommand(Command):
+            def run(self, value): return 10/value
+        
+        assert MyCommand(2).result() == 5
+        assert isinstance(MyCommand(0).exception(), ZeroDivisionError)
+        assert MyCommand(0).result() # This should throw
+
+    @raises(ZeroDivisionError)
+    def test_failing_fallback(sefl):
+        class MyCommand(Command):
+            def run(self, value): return 10/value
+            def fallback(self, e): raise RuntimeError()
+        
+        assert MyCommand(2).result() == 5
+        assert isinstance(MyCommand(0).exception(), ZeroDivisionError)
+        assert MyCommand(0).result() # This should throw
+
+    def test_cleanup(sefl):
+        mutable = []
+        class MyCommand(Command):
+            def run(self): pass
+            def cleanup(self): mutable.append(None)
+        
+        assert not mutable
+        MyCommand().result()
+        assert mutable
+
+    def test_cleanup_after_error(sefl):
+        mutable = []
+        class MyCommand(Command):
+            def run(self): return 1/0
+            def cleanup(self): mutable.append(None)
+        
+        assert not mutable
+        assert MyCommand().exception()
+        assert mutable
+
+    def test_cleanup_after_fallback(sefl):
+        mutable = []
+        class MyCommand(Command):
+            def run(self): return 1/0
+            def fallback(self, e): return 5
+            def cleanup(self): mutable.append(None)
+        
+        assert not mutable
+        assert MyCommand().result() == 5
+        assert mutable
+
+    def test_cleanup_after_fallback_error(sefl):
+        mutable = []
+        class MyCommand(Command):
+            def run(self): return 1/0
+            def fallback(self, e): return 1/0
+            def cleanup(self): mutable.append(None)
+        
+        assert not mutable
+        assert MyCommand().exception()
+        assert mutable
+
+    def test_cleanup_error(sefl):
+        class MyCommand(Command):
+            def run(self): pass
+            def cleanup(self): 1/0
+
+        assert MyCommand().result() is None
+
+    def test_result_twice(self):
+        class MyCommand(Command):
+            def run(self, value): return value
+        
+        cmd = MyCommand(5)
+        assert cmd.result()
+        assert cmd.result()
+
+    @raises(CommandIntegrityError)
+    def test_result_twice(self):
+        class MyCommand(Command):
+            def run(self, value): return value
+        
+        cmd = MyCommand(5)
+        assert cmd.submit()
+        assert cmd.submit()
+
+    @raises(CommandCancelledError)
+    def test_cancle_early(self):
+        class MyCommand(Command):
+            def run(self, value): return value
+        
+        cmd = MyCommand(5)
+        assert cmd.cancel()
+        assert cmd.done()
+        assert cmd.cancelled()
+        assert not cmd.running()
+        cmd.result()
+
+
