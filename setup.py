0d515c8a2c9d6deb87af7c4269188923628a391d
Marcel Hellkamp <marc@gsites.de>
2013-05-16 21:53:53 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+    name='pycopine',
+    version='0.1-dev',
+    packages=['pycopine'],
+    description='Latency and fault tolerance library inspired by Hystrix.',
+    url='https://github.com/defnull/pycopine',
+    author='Marcel Hellkamp',
+    author_email='marc@gsites.de',
+    classifiers= [
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+    ]
+)
