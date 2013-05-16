0857ab12d9adf0673299c859d08c641df4a11f16
Marcel Hellkamp <marc@gsites.de>
2013-05-16 22:55:12 +0200

    Added futures dependency for python 3.1

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
-from distutils.core import setup
+#!/usr/bin/env python
+import sys
+
+extras = {}
+try:
+    from setuptools import setup
+    if sys.version_info < (3, 2):
+        extras['install_requires'] = ['futures']
+except ImportError:
+    from distutils.core import setup
 
 setup(
     name='pycopine',
@@ -6,10 +15,11 @@ setup(
     packages=['pycopine'],
     description='Latency and fault tolerance library inspired by Hystrix.',
     url='https://github.com/defnull/pycopine',
+    download_url='http://pypi.python.org/pypi/pycopine/',
     author='Marcel Hellkamp',
     author_email='marc@gsites.de',
     classifiers= [
-        'Development Status :: 4 - Beta',
+        'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
@@ -17,5 +27,6 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
-    ]
+    ],
+    **extras
 )
