db64dd5c955b845935887342a9267e5017e8cc78
Marcel Hellkamp <marc@gsites.de>
2013-06-12 00:41:24 +0200

    Different approach for RRBucketCounter

--- pycopine/metrics.py
+++ pycopine/metrics.py
@@ -2,24 +2,26 @@ from time import clock
 import threading
 
 class RRBucketCounter(list):
-    def __init__(self, size):
-        for x in range(size):
-            self.append(0)
-        self.last_bucket = int((clock()%1)*len(self))
+    def __init__(self, millis=1000, buckets=10):
+        self.buckets = buckets
+        self.clockfunc = lambda: int(clock()*buckets*millis/1000)
+        self.last_ts = 0
         self.lock = threading.Lock()
     
     def increment(self, n=1):
-        bucket = int((clock()%1)*len(self))
-        self[bucket] += n
         with self.lock:
-            while self.last_bucket != bucket:
-                self[self.last_bucket] = 0
-                self.last_bucket = (self.last_bucket + 1) % len(self)
+            t = self.clockfunc()
+            off = t - len(self.buckets)
+            while self and self[0][0] < off:
+                self.pop(0)
+            if self and self[-1][0] == t:
+                self[-1][1] += n
+            else:
+                self.append([t,n])
         
     def value(self):
-        return sum(self)
+        return sum(v, for t,v in self)
 
     def clear(self):
-        for i in range(len(self)):
-            self[i] = 0
+        del self[:]
 
